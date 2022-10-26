77009f629df38935097e3adf75bbc562a7ff3fb8
rafael-moriya <91164317+rafael-moriya@users.noreply.github.com>
2022-10-26 16:36:52 -0300

    Add files via upload
    arrumando

---
+++ orientado-a-objeto-JS/public/js.js
@@ -0,0 +1,12 @@
+class produto {
+
+    constructor () {
+        
+    }
+    adicionar(){
+    alert('vamos verificar um produto!');
+       //alert do botao adicionar 
+      }
+    }
+    
+    var produto = new produto();
