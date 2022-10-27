a53decc88544b1e57ae2a19a9d1f346c430245da
rafael-moriya <91164317+rafael-moriya@users.noreply.github.com>
2022-10-27 16:32:47 -0300

    Add files via upload
    e é isso

---
+++ js.js
@@ -0,0 +1,18 @@
+class produto {
+
+    constructor () {
+      this.id =0;
+      this.nomeProduto = '';
+      this.valor =0;
+        
+    }
+    adicionar(){
+    alert('vamos verificar um produto!');
+       //alert do botao adicionar 
+      
+      }
+      excluir () {
+        alert('item deletado');
+    }
+  }
+    var produto = new produto();
