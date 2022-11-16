82fa6d7a3042a8b3799f385a79d146eb0222a340
rafael-moriya <91164317+rafael-moriya@users.noreply.github.com>
2022-11-16 17:31:31 -0300

    Add files via upload
    oiii

--- public/js.js
+++ public/js.js
@@ -35,11 +35,52 @@ class planta{
         }else{
             this.idade=o
         }
+<<<<<<< HEAD
+    
+}
+get marca() {
+    return this.marca
+}
+get ano() {
+     return this.ano
+}
+}
+class carroOffRoad extends carro {
+    constructor(pneu, tiporua) {
+        super('', '', '')
+        this.pneu = pneu
+        this.tiporua = tiporua
+=======
+>>>>>>> 1f657e3c3b1906b7a1ec13dd9ebe691deb180456
     }
     get Idade(){
         return this.idade
     }
 
+<<<<<<< HEAD
+        this.tiporua = t
+    }
+    get tiporua() {
+        return this.tiporua
+    }
+}
+// primeiro carro
+let carro1 = new carroOffRoad()
+carro1.setano = 10
+carro1.setmarca = "bmw"
+carro1.setpneu = "aro 24"
+carro1.tiporua = "terrenos aridos"
+console.log("carro1: " + carro1)
+carro1.info()
+// segundo carro
+let carro22 = new carroOffRoad()
+carro2.setano = 2
+carro2.setmarca = "marca"
+carro2.setpneu = "aro 25"
+carro2.tiporua = "terrenos aridos"
+console.log("carro2: " + carro2)
+carro2.info()
+=======
     set Nome(e){
         this.nome = e
     }
@@ -86,3 +127,4 @@ console.log("Nome da planta: " + planta2.setnome)
 planta2.info()
 
 
+>>>>>>> 1f657e3c3b1906b7a1ec13dd9ebe691deb180456
